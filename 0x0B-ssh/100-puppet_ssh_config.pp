22f1f928a73e97f7f8469b8a960a05629ca9212a
eyasu <mesakmegerem@gmail.com>
2023-12-25 04:31:57 -0800

    fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-# Puppet to make changes to our configuration file
+# puppet
 
-file { 'etc/ssh/shh_config':
-	ensure => present,
-
-content =>"
-	#SSH client configuration
-	host*
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no
-	",
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content => "
+    # SSH client configuration
+    Host *
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+  ",
 }
+
