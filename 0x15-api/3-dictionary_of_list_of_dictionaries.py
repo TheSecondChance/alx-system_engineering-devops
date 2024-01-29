8978a1b4088e0f2a085f17d169ea1768d43369ad
TheSecondChance <mesakmegerem@gmail.com>
2024-01-29 10:37:26 -0800

    Add 3-dictionary_of_list_of_dictionaries.py

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Python script to export data in the JSON format."""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    userId = sys.argv[1]
+    feachUrl = "https://jsonplaceholder.typicode.com/"
+    userJson = requests.get(feachUrl + "users/{}".format(userId)).json()
+    userName = userJson.get("username")
+    todos = requests.get(feachUrl + "todos", params={"userId": userId}).json()
+    
+    """Dump to json file format"""
+    with open("{}.json".format(userId), "w") as jsonFile:
+        json.dump({userId: [{
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": userName
+            } for todo in todos]}, jsonFile)
