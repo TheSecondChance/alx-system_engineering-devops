b14303bd934e008e07bf55e964e829228adbb13c
TheSecondChance <mesakmegerem@gmail.com>
2024-01-29 05:36:48 -0800

    Add 0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/bin/usr/python3
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    feachUrl = "https://jsonplaceholder.typicode.com/"
+    employeeId = sys.argv[1]
+    userRsponse = requests.get(feachUrl + "users/{}".format(
+        employeeId
+    ))
+    userResponsJason = userRsponse.json()
+    params = {"userId": employeeId}
+    todosRespons = requests.get(feachUrl + "todos",
+                                params=params)
+    todos = todosRespons.json()
+    taskCompleted = []
+    for todo in todos:
+        if todo.get("completed") is True:
+            taskCompleted.append(todo.get("title"))
+    print("Employee {} is done with task({}/{})".format(
+        userResponsJason.get("name"), len(taskCompleted), len(todos)
+    ))
+    
+    for Completed in taskCompleted:
+        print("\t {}".format(Completed))
