804b09bc9cc54238795092d81c9057d132b117bb
TheSecondChance <mesakmegerem@gmail.com>
2024-02-13 05:57:32 -0800

    Fix userAgent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,15 +5,13 @@ import requests
 
 def number_of_subscribers(subreddit):
     headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' +
-            'AppleWebKit/537.36 (KHTML, like Gecko)' +
-            'Chrome/121.0.0.0 Safari/537.36'
-            }
-    response = requests.get('https://www.reddit.com/r/{:}/about.json'.format(
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' +
+        'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+    }
+    re = requests.get('https://www.reddit.com/r/{:}/about.json'.format(
         subreddit), headers=headers, allow_redirects=False)
-    if response.status_code >= 300:
+    if re.status_code >= 300:
         return 0
-    else:
-        json = response.json()
-        data_dict = json.get('data')
-        return(data_dict.get('subscribers'))
+    json = re.json()
+    data_dict = json.get('data')
+    return(data_dict.get('subscribers'))
