b4d0dbc2c1af159a0fc3f455c44538b5256557dd
TheSecondChance <mesakmegerem@gmail.com>
2024-02-22 04:30:38 -0800

    Add 0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# This for increase the open file limit for to 4096
+exec { 'change ULIMIT':
+  command => "sed -i 's/15/4096/g' /etc/default/nginx ; service nginx restart",
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
